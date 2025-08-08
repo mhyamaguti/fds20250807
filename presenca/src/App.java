f08695a52c42ff61f6d600dee2fb0a00568b5be1
MURILO DA SILVA SOUZA <murilo.souza003@edu.pucrs.br>
2025-08-08 01:20:12 +0000

    MuriloDaSilvaSouza

--- presenca/src/App.java
+++ presenca/src/App.java
@@ -2,5 +2,8 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Presenca da aula 7/8/2025");
         // Insira uma linha que mostre o seu nome na tela
+
+
+        System.out.println("Murilo da Silva Souza");
     }
 }
