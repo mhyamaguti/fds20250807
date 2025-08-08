33cbc9e478e7abbddd022df31e2d68d10ec29b58
Raul Sjlender Monson <raulsjl12@gmail.com>
2025-08-07 22:21:48 -0300

    Update App.java

--- presenca/src/App.java
+++ presenca/src/App.java
@@ -1,6 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Presenca da aula 7/8/2025");
-        // Insira uma linha que mostre o seu nome na tela
+        System.out.println("Raul Sjlender Monson");
     }
 }
+
