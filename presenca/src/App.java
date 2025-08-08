7d8696f9a5d42f8b3d9a9258ca0b9fae31f52d7e
Gabriel Maxwell <103340783+Gabriel-Maxwell@users.noreply.github.com>
2025-08-08 01:20:27 +0000

    Presenca GabrIelMaxwell

--- presenca/src/App.java
+++ presenca/src/App.java
@@ -2,5 +2,18 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Presenca da aula 7/8/2025");
         // Insira uma linha que mostre o seu nome na tela
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        //Gabriel Maxwell
     }
 }
