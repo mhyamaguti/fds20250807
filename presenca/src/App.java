1d19a8a44903e8fa14aa9c49ae42e50c3bf594d2
Rafael Richter Madeira <rafael.richter@edu.pucrs.br>
2025-08-08 01:27:09 +0000

    rafael

--- presenca/src/App.java
+++ presenca/src/App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Presenca da aula 7/8/2025");
         // Insira uma linha que mostre o seu nome na tela
+        // rafael richter madeira
     }
 }
